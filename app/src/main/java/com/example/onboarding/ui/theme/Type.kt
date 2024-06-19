386728551dbd3ed882c46351fbf44237063eb242
tereshkevich1 <tereshkevich.cergey@mail.ru>
2024-06-19 16:43:08 +0300

    added resources (fonts, vectors)

--- app/src/main/java/com/example/onboarding/ui/theme/Type.kt
+++ app/src/main/java/com/example/onboarding/ui/theme/Type.kt
@@ -1,12 +1,21 @@
 package com.example.onboarding.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.onboarding.R
 
 // Set of Material typography styles to start with
+
+val Roboto = FontFamily(
+    Font(R.font.roboto, FontWeight.Normal),
+    Font(R.font.roboto_bold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -14,21 +23,21 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 1.sp,
+        color = Color.White
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.5.sp,
+        color = Color.White,
     )
-    */
 )
